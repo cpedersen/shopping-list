6c907fa894ae0456a3c8c5c17614a09f1a3de859
Catherine Pedersen <cathy.pedersen@gmail.com>
2019-10-23 09:39:47 -0400

    Fix empty add in html; fix add & toggle items in js

--- index.js
+++ index.js
@@ -27,7 +27,16 @@ function addItems() {
 
         //Build the html for the new item, then push to the new array 
         item.push($(
-        `<ul class="shopping-list"> <li><span class="shopping-item">${text}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`));
+        `<li><span class="shopping-item">${text}
+            </span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+            </button><button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+            </button>
+            </div>
+            </li>`));
 
         //Append the html to the bottom of the existing shopping-list 
         $('.shopping-list').append(item);
@@ -37,45 +46,18 @@ function addItems() {
 
 function toggleItems() {
 
-    //shopping-item-toggle'
-    $('.button-label').on('click', function(event) {
-        
+    $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+
         console.log("Toggling item");
         event.preventDefault();
-        //console.log( $( this ).text() );
 
         //Get item that user is interacting with
-        const target = $(event.currentTarget).closest('.button-label');
-        //const target = $(event.currentTarget).closest('.shopping-item');
-        const text = $(event.currentTarget).closest('.shopping-item').text();
-        console.log("text = " + text);
+        const target = $(event.currentTarget).closest('li').find('.shopping-item');
+        console.log("target = " + target);
 
-        //Create empty array 
-        const item = [];
-
-        //Build the html for strikethrough, then set it as an attribute - TODO
-        //TODO - use attr to overwrite the html?
-        item.push($(
-            `<ul class="shopping-list"> <li><span class="shopping-item shopping-item__checked">${text}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`));
-        
-        //Test that we can toggle this item
-        //TODO - how do a strikethoug the text?
+        //Strike through grocery item text
         target.toggleClass('shopping-item__checked');
-        //text.toggleClass('.shopping-item__checked');
 
-        //Test that we can delete this item
-        //target.removeClass('.shopping-item');
-
-        //Check if item is a 'check' button or a 'delete' button
-        //if (target === "shopping-item-toggle") {
-        //    console.log("Check button selected");
-        //    target.toggleClass('.shopping-item-toggle');
-        //} else if (target === "shopping-item-delete) {
-        //    console.log("Delete button selected");
-        //    target.toggleClass('.shopping-item-delete');
-        //} else {
-        //    console.log("No button selected");
-        //}
     });
 }
 
